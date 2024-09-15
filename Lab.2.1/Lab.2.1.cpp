a4af2c2bb885740f6c2937d11a0fe94ea98e5491
Anya Stefurak <anyutastefurak@gmail.com>
2024-09-15 14:51:38 +0300

    first master commit

---
+++ Lab.2.1/Lab.2.1.cpp
@@ -0,0 +1,29 @@
+﻿// Lab_02.cpp
+// < Стефурак Анна >
+// Лабораторна робота No 2.1
+// Лінійні програми.
+// Варіант 14
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double z1;
+    double z2;
+
+    cout << "a=?";
+    cin >> a;
+
+    z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+    z2 = tan(2 * a) + 1 / cos(2 * a); // sec(x) = 1 / cos(x)
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
